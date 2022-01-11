e3a69f5b9ec550619b160b46ec611caa6fd692a9
andeztaz7 <81946159+andeztaz7@users.noreply.github.com>
2022-01-11 14:43:58 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,139 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>INFORMATIKA PESAT</title>
+</head>
+<body>
+    <img id="header-img" src="" alt="">
+    <div class="nav">
+        <a class="navbar" href="#">
+            <img src="https://sekolahpesatbogor.com/wp-content/uploads/2021/08/Logo-SMK-Polos.png" alt="" width="70" height="24">
+        </a>
+        <a class="active" href="#home">Home</a>
+        <a href="#reg">Profile School</a>
+        <a href="#reg">PPDB</a>
+    </div>
+    <div class="depan">
+        <h1>WELCOME TO INFORMATIKA PESAT</h1>
+    </div>
+    <div class="profil">
+        <div class="kelas">
+            <h3>RPL</h3>
+                <p>Software Engineering atau SE adalah satu bidang profesi yang mendalami cara-cara pengembangan perangkat lunak termasuk pembuatan, pemeliharaan, manajemen organisasi pengembangan perangkat lunak dan manajemen kualitas</p>
+            
+        </div>
+        <div class="kelas">
+            <h3>TKJ</h3>
+                <p>TKJ (Teknik Komputer dan Jaringan) adalah ilmu berbasis Teknologi Informasi dan Komunikasi terkait kemampuan algoritma, dan pemrograman komputer, perakitan komputer, perakitan jaringan komputer, dan pengoperasian perangkat lunak, dan internet.</p>
+        </div>
+        <div class="kelas">
+            <h3>MM</h3>
+                <p>Multimedia adalah media yang menggabungkan dua unsur atau lebih media yang terdiri dari teks, grafik, gambar, foto, audio, dan animasi secara terintegrasi.</p>
+        </div>
+    </div>
+    <div class="all">
+        <div class="registrasi">
+            <h1>REGISTRASI</h1>
+        </div>
+    <form>
+        <div class="formulir">
+        <div class="mb-3">
+          <label for="exampleInputEmail1" class="form-label">Nama</label>
+          <input type="Nama" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+          <div id="emailHelp" class="form-text">Email will be keep by ourself.</div>
+        </div>
+        <div class="mb-3">
+          <label for="exampleInputPassword1" class="form-label">TTL</label>
+          <input type="TTL" class="form-control" id="exampleInputPassword1">
+        </div>
+        <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Warga Negara</label>
+            <input type="Warga Negara" class="form-control" id="exampleInputPassword1">
+          </div>
+          <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Alamat</label>
+            <input type="Alamat" class="form-control" id="exampleInputPassword1">
+          </div>
+          <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Email</label>
+            <input type="Email" class="form-control" id="exampleInputPassword1">
+          </div>
+          <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Nomor HP</label>
+            <input type="Nomor HP" class="form-control" id="exampleInputPassword1">
+          </div>
+          <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Asal SMP</label>
+            <input type="Asal SMP" class="form-control" id="exampleInputPassword1">
+          </div>
+          <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Nama Ayah</label>
+            <input type="Nama Ayah" class="form-control" id="exampleInputPassword1">
+          </div>
+          <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Nama Ibu</label>
+            <input type="Nama Ibu" class="form-control" id="exampleInputPassword1">
+          </div>
+          <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Penghasilan Kedua Orang tua</label>
+            <input type="Penghasilan Kedua Orang Tua" class="form-control" id="exampleInputPassword1">
+          </div>
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        <div class="mb-3 form-check">
+          <input type="checkbox" class="form-check-input" id="exampleCheck1">
+          <label class="form-check-label" for="exampleCheck1">RE-CHECK?</label>
+        </div>
+        <button type="submit" class="btn btn-primary">REGIST</button>
+        </div>
+    </form>
+    </div>
+</body>
+</html>
